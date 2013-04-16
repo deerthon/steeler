2fe789569733a39498eaba8d3f7eef2f5bd7f02d
lindosekai <lindosekai@rockbell.(none)>
2013-04-15 19:52:18 -0500

    El inicio

---
+++ steel.py
@@ -0,0 +1,11 @@
+def prompt():
+    datain=""
+    while(datain!="exit"):
+        datain =  raw_input(">> ")
+
+print "+------------------------------------------------------------+"
+print "+        -----     Wellcome to STEELER     -----             +"
+print "+------------------------------------------------------------+"
+print "type \'exit\' without quotes to logout..."
+print 
+prompt()
